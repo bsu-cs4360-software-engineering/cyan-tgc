fa4dbcc6bb66c86730eebc4ef590383ebd455efb
JVeldkamp <ericwarfish212@gmail.com>
2024-09-24 04:22:31 -0500

    Added framework for hand system

---
+++ TGC Project/Assets/DrawBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DrawBehavior : MonoBehaviour
+{
+    public Button Draw;
+    private GameObject CardPos1;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = Draw.GetComponent<Button>();
+        btn.onClick.AddListener(onClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void onClick()
+    {
+        CardPos1 = GameObject.Find("CardPos1");
+        if (CardPos1.transform.childCount < 1)
+        {
+            Debug.Log("No Child!");
+        }
+        
+    }
+}
